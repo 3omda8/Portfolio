3919de6c8eaf7e43dd82b4665fa42fc356110b6e
3omda8 <mohameddawood664@gmail.com>
2024-05-11 12:47:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let active = true;
+function toggle() {
+  if (active) {
+    document.getElementsByClassName("skills")[0].classList.add("visible");
+    document.getElementsByClassName("btn")[0].innerHTML = "Hide";
+  } else {
+    document.getElementsByClassName("skills")[0].classList.remove("visible");
+    document.getElementsByClassName("btn")[0].innerHTML = "Skills";
+  }
+  active = !active;
+}
+
+//Slider
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 3 seconds
+}
